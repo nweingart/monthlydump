7fdb3b5a400ccf0dd666f9392b1a65e225b331e1
nweingart <76226527+nweingart@users.noreply.github.com>
2022-11-16 12:15:21 -0500

    updated package json and started on multiselect component

--- monthlyupdate/screens/update/UpdateSelect.js
+++ monthlyupdate/screens/update/UpdateSelect.js
@@ -1,125 +1,84 @@
-import React from 'react'
-import {View, Text, StyleSheet, TouchableOpacity, TextInput, Image} from 'react-native'
-import Ionicons from "@expo/vector-icons/Ionicons";
-import {useNavigation} from "@react-navigation/native";
-import SmallLogo from '../../assets/logoSimple.png'
-import {useDispatch} from "react-redux";
-import { setUpdateField1, setUpdateField2, setUpdateField3, setUpdateField4, setUpdateField5 } from '../../redux/redux'
+import React, { useState } from 'react'
 
-const UpdateSelect = () => {
-  const [items, setItems] = React.useState('')
+// component imports
+import {Text, TouchableOpacity, View} from 'react-native'
+import SelectBox from 'react-native-multi-selectbox'
 
-  const dispatch = useDispatch()
-  const navigation = useNavigation()
+// logic imports
+import { xorBy } from 'lodash'
+import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
-  const handleBack = () => {
-    navigation.goBack()
-  }
 
-  const handleNext = () => {
-    dispatch(setUpdateField1(items[0]))
-    dispatch(setUpdateField2(items[1]))
-    dispatch(setUpdateField3(items[2]))
-    dispatch(setUpdateField4(items[3]))
-    dispatch(setUpdateField5(items[4]))
-    navigation.navigate('UpdateField1')
-  }
+const OPTIONS = [
+  {
+    item: 'Family',
+    id: 'FAM',
+  },
+  {
+    item: 'Friends',
+    id: 'FRI',
+  },
+  {
+    item: 'Relationship',
+    id: 'REL',
+  },
+  {
+    item: 'Fitness',
+    id: 'FIT',
+  },
+  {
+    item: 'Health',
+    id: 'HEA',
+  },
+  {
+    item: 'Career',
+    id: 'CAR',
+  },
+  {
+    item: 'Hobbies',
+    id: 'HOB',
+  },
+  {
+    item: 'Travel',
+    id: 'TRV',
+  },
+]
 
-  const addItem = input => {
-    setItems([...items, input])
-  }
+const onFinish = () => {
 
-  console.log(items)
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.backButtonWrapper}>
-        <TouchableOpacity onPress={handleBack}>
-          <Ionicons name="arrow-back-outline" size='25' />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.logoWrapper}>
-        <Image source={SmallLogo} />
-      </View>
-      <View style={styles.topTextWrapper}>
-        <Text style={styles.topText}>Customize your update</Text>
-      </View>
-      <View>
-        <TextInput
-          style={styles.textBox}
-          multiline={true}
-          placeholder="start typing"
-          value={items}
-          onChangeText={text => addItem(text)}
-          autoCapitalize="sentences"
-        />
-      </View>
-      <View style={styles.nextButtonWrapper}>
-        <TouchableOpacity style={styles.nextButton} onPress={handleNext}>
-          <Text style={styles.nextButtonText}>Next</Text>
-          <Ionicons name="arrow-forward-outline" size='25' />
-        </TouchableOpacity>
-      </View>
-    </View>
-  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ACECC2',
-    padding: 25,
-  },
-  logoWrapper: {
-    position: 'absolute',
-    top: '10%',
-  },
-  backButtonWrapper: {
-    position: 'absolute',
-    top: '7.5%',
-    left: '7.5%',
-  },
-  topTextWrapper:{
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
-    backgroundColor: 'white',
-    width: '95%',
-    height: '5%',
-    position: 'absolute',
-    top: '20%',
-  },
-  topText: {
-    color: 'black',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-  textBox: {
-    padding: 20,
-    height: 400,
-    width: 300,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: 'black',
-    backgroundColor: 'white',
-  },
-  nextButtonWrapper: {
-    position: 'absolute',
-    top: '95%',
-    right: '10%',
-  },
-  nextButton: {
-    flexDirection: 'row',
-  },
-  nextButtonText: {
-    color: 'black',
-    fontWeight: '700',
-    fontSize: 16,
-    marginRight: 10,
-    marginTop: 2.5,
+const UpdateSelect = () => {
+  const [selectedTeams, setSelectedTeams] = useState([])
+  const dispatch = useDispatch()
+  const navigation = useNavigation()
+  return (
+    <View style={{ padding: 25, backgroundColor: '#ACECC2', height: '100%', width: '100%' }}>
+      <View style={{ height: 40 }} />
+      <Text style={{ fontSize: 20, paddingBottom: 10 }}>Select Updates</Text>
+      <SelectBox
+        multiOptionContainerStyle={{ backgroundColor: 'white', borderRadius: 10 }}
+        multiOptionsLabelStyle={{ color: 'black', padding: 10 }}
+        label="Select multiple"
+        options={OPTIONS}
+        selectedValues={selectedTeams}
+        onMultiSelect={onMultiChange()}
+        onTapClose={onMultiChange()}
+        isMulti
+      />
+      <Text>You have {selectedTeams.length} / out of 5 items selected!</Text>
+      <Text style={{ color: 'red'}}>{ selectedTeams.length > 5 ? 'You may only select 5 items per update!' : null}</Text>
+      <TouchableOpacity disabled={selectedTeams.length > 5} style={{ backgroundColor: 'black'}}>
+        <Text style={{ color: 'white'}}>Next</Text>
+      </TouchableOpacity>
+    </View>
+  )
+
+  function onMultiChange() {
+    return (item) => setSelectedTeams(xorBy(selectedTeams, [item], 'id'))
   }
-})
+
+}
 
 export default UpdateSelect
